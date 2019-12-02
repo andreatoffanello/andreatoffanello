b020bb4832cc250896e3a19b910113db725c734f
Andrea Toffanello <andrea.toffanello@gmail.com>
2019-12-02 18:48:30 +0100

    updates

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,40 +1,115 @@
-//FORM
-
-function loadform() {
-
-	$('#jobform').submit(function(e){
-	    e.preventDefault();
-	    $.ajax({
-	        url:'https://hive-engine.herokuapp.com/serendpt/jobs_email.json',
-	        type:'post',
-	        data:$('#jobform').serialize(),
-	        success:function(){
-	          $('#jobform').addClass('nodisplay');
-	        }
-	    });
-	});
-}
-
-
 $(document).ready(function() {
 
+	// CURSOR
+
+function cursor() {
+
+	document.body.style.cursor = 'none';
+	/* why we do this here and not in css?
+	 * If the user dosn't have javascript onboard he
+	 * don't see any cursor by default and that's shit
+	 */
+
+	var cursor = document.createElement( 'div' );
+	cursor.classList.add( 'cursor' );
+	document.body.appendChild( cursor );
+
+	var follow = document.createElement( 'div' );
+	follow.classList.add( 'follow' );
+	document.body.appendChild( follow );
+
+	function move( obj, event ) {
+	    obj.style = '';
+	    obj.style.top = event.clientY + 'px';
+	    obj.style.left = event.clientX + 'px';
+	}
+
+	if ( cursor ) {
+	    window.addEventListener( 'mousemove', function( event ) {
+
+	        // legend ;)
+	        var e = event;
+	        var t = e.target;
+	        var f = follow;
+	        var c = cursor;
+
+	        // rambazamba
+	        if ( (t == $('.link')) || (t == $('p a')) ) {
+	            // c.style.backgroundColor = 'transparent';
+
+	            // f.style.top = t.offsetTop + 'px';
+	            // f.style.left = t.offsetLeft + 'px';
+	            // f.style.width = t.clientWidth + 'px';
+	            // f.style.height = t.clientHeight + 'px';
+
+	            // if ( t.classList.contains( 'button--round' ) )
+	            //     f.style.borderRadius = '50%';
+
+	            var aEl = $(t)
+
+				var offset = $(aEl).offset();
+				var width = $(aEl).width();
+				var height = $(aEl).height();
+
+				var centerX = offset.left + width / 2;
+				var centerY = offset.top + height / 2;
+
+	            $( f, c ).css({
+	            	'top' : centerY,
+	            	'left' : centerX
+	            });
 
 
-//
-	loadform();
+	            $( f, c ).addClass( 'on_hover' );
+
+	        }
+
+
+	        //if ( t.tagName == 'A' )
+
+
+	        else {
+	            move( c, e );
+	            move( f, e );
+	            //$(f).removeClass( 'on_hover' );
+	        }
+	    });
+	}
+
+	$('a, .next, .prev').mouseenter( function() {
+		$( '.follow').addClass( 'on_hover' );
+		$( '.cursor').addClass( 'on_hover' );
+
+	});
+
+	$('a, .next, .prev').mouseleave( function() {
+		$( '.cursor').removeClass( 'on_hover' );
+		$( '.follow').removeClass( 'on_hover' );
+	});
+
+	$(document).on('mousemove', function(e){
+	    $(cursor).css({
+	        left:  e.pageX,
+	        top:   e.pageY - $('body').scrollTop()
+	    });
+	});
+
+};
+
+cursor();
 
 //BARBA//
 
 	Barba.Pjax.start();
 
-//LOADER FADEOUT//	
-	
+//LOADER FADEOUT//
+
 	setTimeout(function() {
 
 		$(".loader-container").fadeOut(1500, "swing");
 
 		$(".main_img").removeClass("hidden");
-	
+
 	}, 2000);
 
 
@@ -59,7 +134,7 @@ $(document).ready(function() {
 	     */
 
 	    return $(this.oldContainer).animate({ opacity: 0 }).promise();
-	  
+
 	  },
 
 	  fadeIn: function() {
@@ -95,7 +170,7 @@ $(document).ready(function() {
 					maploading()
 				}
 
-				loadform();
+				// inserire qui funzione
 
 			}()
 
@@ -117,70 +192,4 @@ $(document).ready(function() {
 	  return FadeTransition;
 	};
 
-
-// Hide Header on on scroll down
-
-	var prevScrollpos = window.pageYOffset;
-	
-	window.onscroll = function() {
-	
-	  var currentScrollPos = window.pageYOffset;
-	
-	  if (prevScrollpos > currentScrollPos) {
-	  
-	  	$("nav").css("top", "0");
-	  
-	  } else {
-	  
-	    $("nav").css("top", "-60px");
-	  
-	  }
-	  
-	  prevScrollpos = currentScrollPos;
-
-
-	  // Hide Header on on scroll down
-
-	  var scroll = $(window).scrollTop();
-	
-	  if (scroll > 15) {
-	    $("nav").addClass("scrolled");
-	  }
-
-	  else {
-	  	$("nav").removeClass("scrolled");
-	  }
-	
-	}
-
-	$( ".menu_toggle" ).click(function() {
-	  $( this ).toggleClass( "open" );
-	  $( ".menu_mobile" ).toggleClass( "open" );
-	  $("body").toggleClass( "menu_open" );
-
-	});
-
-	$( ".menu_mobile a, .nav-logo a" ).click(function() {
-	  $( ".menu_toggle" ).removeClass( "open" );
-	  $( ".menu_mobile" ).removeClass( "open" );
-	  $( "body" ).removeClass( "menu_open" );
-	});	
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
